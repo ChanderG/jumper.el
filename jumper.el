3eb44fd78fa7cd0cbc8b35a86cc7da19b81079ad
ChanderG <mail@chandergovind.org>
2024-04-16 11:38:40 +0530

    remove select and add buffer
    adding current buffer seems more than enough in real usuage
    let user use whatever mechanism they want to actually navigate buffers

--- jumper.el
+++ jumper.el
@@ -6,15 +6,8 @@
   (let ((menu (mapcar
                    (lambda (x) (format "%c %s" (car x) (cadr x)))
                    jumper-buffers))
-        (footer "[.] Add current; [j] Add; [J] Clear all; [-x] to remove buffer x"))
+        (footer "[.] Add current; [J] Clear all; [-x] to remove buffer x"))
     (format "Buffers: \n %s\n%s" menu footer)))
-
-(defun jumper-search-add ()
-  (let ((buf (helm-mini)))
-    ;; to undo the effect of helm-mini
-    ;; neither save-current-buffer nor save-excursion work here for some reason
-    (previous-buffer)
-    (jumper-add buf)))
 
 (defun jumper-add (buf)
   (let ((ch (downcase (string-to-char (buffer-name buf)))))
@@ -35,13 +28,10 @@
   (interactive)
   (let* ((msg (jumper-msg))
          (inp (read-char msg)))
-    (cond ((eq inp ?j) (jumper-search-add))
-          ((eq inp ?J) (setq jumper-buffers nil))
+    (cond ((eq inp ?J) (setq jumper-buffers nil))
           ((eq inp ?.) (jumper-add (current-buffer)))
           ((eq inp ?-) (jumper-remove (read-char "Jumper to remove: ")))
           ((eq inp ?\e) nil)
-          (t (jumper-jump inp))
-          )
-    ))
+          (t (jumper-jump inp)))))
 
 (define-key evil-normal-state-map "j" 'jumper-trigger)
